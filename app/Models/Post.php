c9b2de207b11ac3f611ff41f574880a6fa9df929
wenderson guedes <wendersonguedes6@gmail.com>
2022-04-04 13:42:10 -0300

    Model para os posts

---
+++ app/Models/Post.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // Indicando quais campos podem ter atribuição em massa (valores inseridos de uma só vez).
+    protected $fillable = ['title', 'content'];
+
+}
