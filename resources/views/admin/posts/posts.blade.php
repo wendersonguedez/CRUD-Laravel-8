9e8907b9efb027d2bcc1d8eda5b991fbad862048
wenderson guedes <wendersonguedes6@gmail.com>
2022-04-04 16:47:26 -0300

    View para listagens dos posts já criados

--- resources/views/admin/posts/posts.blade.php
+++ resources/views/admin/posts/posts.blade.php
@@ -1,7 +1,18 @@
 <a href="{{ route('posts.create') }}">Criar novo post</a>
 <hr>
+
+{{-- Verificando se existe uma session flash (sessão temporária). Caso exista, seu valor é impresso para o  usuário. --}}
+@if (session('message'))
+    <div style="color: green">
+        {{ session('message') }}
+    </div>
+@endif
+
 <h1>Posts</h1>
 
 @foreach ($posts as $post)
-    <p>{{ $post->title }}</p>
+    <p>
+        {{ $post->title }}
+        [ <a href="{{ route('posts.show', $post->id) }}">Ver detalhes</a> ]
+    </p>
 @endforeach
