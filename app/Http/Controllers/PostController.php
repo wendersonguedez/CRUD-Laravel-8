e4bd5cf18cbc513a75613ea884097d03c7bb0a53
wenderson guedes <wendersonguedes6@gmail.com>
2022-04-04 16:45:38 -0300

    Métodos de inserção e exclusão de posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUpdatePost;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
@@ -20,7 +21,8 @@ class PostController extends Controller
         return view('admin.posts.create');
     }
 
-    public function store(Request $request)
+    // StoreUpdatePost é um objeto que realizar as validações dos campos.
+    public function store(StoreUpdatePost $request)
     {
         /*
         Capturando todos os registros que foram enviados pelo formulário.
@@ -40,6 +42,38 @@ class PostController extends Controller
         /* Inserindo todos os dados automaticamente em seus respectivos campos da tabela. */
         Post::create($request->all());
 
+        // Ao finalizar a inserção dos dados, o usuário é redirecionado para a página principal.
         return redirect()->route('posts.index');
+    }
+
+    public function show($id)
+    {
+        /* 
+        Capturando o post pelo seu ID, através da model 'Post'. where() verifica se o valor da coluna 
+        'id', é igual ao valor da variável $id.
+        
+        $post = Post::where('id', $id)->first();
+        */
+
+        // O método find() recupera um item diretamente pelo seu ID. Caso o valor de $post seja null, ou seja, nenhum post encontrado, o usuário é redirecionado para a página principal.
+        if (!$post = Post::find($id)) {
+            return redirect()->route('posts.index');
+        }
+
+
+        return view('admin.posts.show', compact('post'));
+    }
+
+    public function destroy($id)
+    {
+        if (!$post = Post::find($id))
+            return redirect()->route('posts.index');
+
+        // Excluindo o registro de um post do banco.
+        $post->delete();
+
+        return redirect()
+            ->route('posts.index')
+            ->with('message', 'Post deletado com sucesso!'); // Messagem para o usuário através de uma session flash (sessão temporária).
     }
 }
