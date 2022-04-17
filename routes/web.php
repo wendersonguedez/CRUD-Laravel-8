45349a240fa0f39bc060c8197ed1339c427b09d2
wenderson guedes <wendersonguedes6@gmail.com>
2022-04-17 19:35:58 -0300

    Rotas da aplicação

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,11 @@ use App\Http\Controllers\{
 };
 use Illuminate\Support\Facades\Route;
 
-Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
-
-Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
-
-Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-
+Route::any('/posts/search', [PostController::class, 'search'])->name('posts.search');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
-
+Route::get('/posts/edit/{id}', [PostController::class, 'edit'])->name('posts.edit');
+Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+Route::put('/posts/{id}', [PostController::class, 'update'])->name('posts.update');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('/posts', [PostController::class, 'posts'])->name('posts.index');
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
