7828bb2d733b69eac303459f58877e9b82511be4
wenderson guedes <wendersonguedes6@gmail.com>
2022-04-17 19:35:19 -0300

    View para exibir um post com maiores detalhes

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,13 +1,19 @@
-<h1>Detalhes do post {{ $post->title }}</h1>
+@extends('admin.layouts.app')
 
-<ul>
-    <li><strong>Título: </strong>{{ $post->title }}</li>
-    <li><strong>Conteúdo: </strong>{{ $post->content }}</li>
-</ul>
+@section('title', 'Detalhes do Post')
 
-<form action="{{ route('posts.destroy', $post->id) }}" method="post">
-    @csrf
-    {{-- Passando de forma oculta o método de envio desse formulário --}}
-    <input type="hidden" name="_method" value="DELETE">
-    <button type="submit">Deletar o post: {{ $post->title }}</button>
-</form>
+@section('content')
+    <h1>Detalhes do post {{ $post->title }}</h1>
+
+    <ul>
+        <li><strong>Título: </strong>{{ $post->title }}</li>
+        <li><strong>Conteúdo: </strong>{{ $post->content }}</li>
+    </ul>
+
+    <form action="{{ route('posts.destroy', $post->id) }}" method="post">
+        @csrf
+        {{-- Passando de forma oculta o método de envio desse formulário --}}
+        <input type="hidden" name="_method" value="DELETE">
+        <button type="submit">Deletar o post: {{ $post->title }}</button>
+    </form>
+@endsection
