8437f7cbcaee144fe355fceea10864a00f5aa32b
wenderson guedes <wendersonguedes6@gmail.com>
2022-04-17 19:34:44 -0300

    View para criação dos posts

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,19 +1,12 @@
-<h1>Cadastrar novo post</h1>
+@extends('admin.layouts.app')
 
-{{-- Caso tenha algum erro, ele é exibido para o usuário. any() por default é false, caso true, indica que possui erros. --}}
-@if ($errors->any())
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
-<form action="{{ route('posts.store') }}" method="POST">
-    @csrf {{-- Token para validação de formulário --}}
+@section('title', 'Criar Novo Post')
 
-    {{-- old('') cria uma sessão temporária com o valor preenchido pelo usuário, que em casos de refresh da página, o valor não ser perdido. --}}
-    <input type="text" name="title" id="title" placeholder="Título" value="{{ old('title') }}">
-    <textarea name="content" id="content" cols="30" rows="4" placeholder="Conteúdo">{{ old('content') }}
-    </textarea>
-    <button type="submit">Enviar</button>
-</form>
+
+@section('content')
+    <h1>Cadastrar novo post</h1>
+
+    <form action="{{ route('posts.store') }}" method="POST" enctype="multipart/form-data">
+        @include('admin.posts._partials.form')
+    </form>
+@endsection
